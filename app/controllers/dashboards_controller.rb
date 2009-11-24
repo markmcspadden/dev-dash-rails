b50e5dc16fee06bd4aa70a2158923955ca3e3381
Mark McSpadden <markmcspadden@gmail.com>
2009-11-24 16:56:20 -0600

    Add live lookups for dev-dash-core

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,11 +2,15 @@ class DashboardsController < ApplicationController
   
   def index
     @rcr = DevDash::RunCodeRun.new
-    @rcr.file_path = "#{RAILS_ROOT}/lib/dev-dash-core/test/fixtures/run_code_run.json"
+    @rcr.uri = "http://runcoderun.com/api/v1/json/markmcspadden"
     @rcr.fetch
     
     @ht = DevDash::Hoptoad.new
-    @ht.file_path = "#{RAILS_ROOT}/lib/dev-dash-core/test/fixtures/hoptoad.xml"
+    
+    token = "fa7dce9f977a4d9199de3f191ea9d246e64d7990"
+    
+    @ht.uri = "http://markmcspadden.hoptoadapp.com/errors.xml?auth_token=#{token}"
+    @ht.projects_uri = "http://markmcspadden.hoptoadapp.com/projects.xml?auth_token=#{token}"
     @ht.fetch
   end
   
